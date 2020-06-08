4c4bd54e00682e30e82d77c65b72fafd41b864fb
Simon Moses Onazi <syflex360@gmail.com>
2020-06-08 10:33:22 +0100

    allow login

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -108,9 +108,9 @@ export default ({
       }
   },
 
-  mounted() {
-    // this. login();
-    this.getAllData();
+  async mounted() {
+    await this. login();
+    await this.getAllData();
   },
 
   methods: {
