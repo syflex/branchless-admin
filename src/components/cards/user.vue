be34654389324e0e9c1199030bdd8fea378ed9d1
Simon Moses Onazi <syflex360@gmail.com>
2020-07-09 23:14:15 +0100

    updated chart and user

--- src/components/cards/user.vue
+++ src/components/cards/user.vue
@@ -6,16 +6,20 @@
         class="transparent card-border shadow-5 hoverable shadow-transition"
       >
         <q-card-section class="column flex-center">
+           <div class="text-bold row flex-center">
+            <q-icon name="fa fa-users" class="q-mr-md" />
+            <div>Users</div>
+          </div>
           <div class="text-h6 text-bold">
             <span class="text-caption text-grey-8">total</span>
             {{ leftFunc(users, {field: 'createdAt', value: ''}, {}, filter) }} | {{ rightFunc(users, {field: 'createdAt', value: ''}, {}, filter) }}
             <span class="text-caption text-grey-8">today</span>
-            <!-- <q-icon name="fa fa-chart-line" /> -->
           </div>
-          <div class="text-bold row flex-center">
-            <q-icon name="fa fa-users" class="q-mr-md" />
-            <div>Users</div>
+          <div>
+             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="35px" fill="#FFF"><path d="M117.65 277.65c6.25 6.25 16.38 6.25 22.63 0L192 225.94l84.69 84.69c6.25 6.25 16.38 6.25 22.63 0L409.54 200.4l29.49 29.5c15.12 15.12 40.97 4.41 40.97-16.97V112c0-8.84-7.16-16-16-16H363.07c-21.38 0-32.09 25.85-16.97 40.97l29.5 29.49-87.6 87.6-84.69-84.69c-6.25-6.25-16.38-6.25-22.63 0l-74.34 74.34c-6.25 6.25-6.25 16.38 0 22.63l11.31 11.31zM496 400H48V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16z"/></svg>
+             <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="35px" fill="#FFF"><path d="M180.69 246.62c6.25 6.25 16.38 6.25 22.63 0L288 161.94l87.6 87.6-29.5 29.49c-15.12 15.12-4.41 40.97 16.97 40.97H464c8.84 0 16-7.16 16-16V203.07c0-21.38-25.85-32.09-40.97-16.97l-29.49 29.5-110.23-110.22c-6.25-6.25-16.38-6.25-22.63 0L192 190.06l-51.72-51.72c-6.25-6.25-16.38-6.25-22.63 0l-11.31 11.31c-6.25 6.25-6.25 16.38 0 22.63l74.35 74.34zM496 400H48V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16z"/></svg> -->
           </div>
+
         </q-card-section>
 
         <q-separator color="teal-10" />
@@ -24,13 +28,13 @@
           <div class="text-center">
             <div>Male</div>
             <div>
-              {{ sortGeneralField(users, 'gender', 'male') }} | {{ sortToday(users, {field: 'createdAt', value: ''}, {field: 'gender', value: 'male'}).length }}
+              {{ leftFunc(users, {field: 'gender', value: 'male'}, {}, filter) }} | {{ rightFunc(users, {field: 'createdAt', value: ''}, {field: 'gender', value: 'male'}, filter) }}
             </div>
           </div>
           <div class="text-center">
             <div>Female</div>
             <div>
-              {{ sortGeneralField(users, 'gender', 'female') }} | {{ sortToday(users, {field: 'createdAt', value: ''}, {field: 'gender', value: 'female'}).length }}
+              {{ leftFunc(users, {field: 'gender', value: 'female'}, {}, filter) }} | {{ rightFunc(users, {field: 'createdAt', value: ''}, {field: 'gender', value: 'female'}, filter ) }}
             </div>
           </div>
         </q-card-section>
@@ -39,13 +43,13 @@
           <div class="text-center">
             <div>Active</div>
             <div>
-              {{ sortGeneralField(users, 'status', 'active') }} | {{ sortToday(users, {field: 'createdAt', value: ''}, {field: 'status', value: 'active'}).length }}
+              {{ leftFunc(users, {field: 'status', value: 'active'}, {}, filter) }} | {{ rightFunc(users, {field: 'createdAt', value: ''}, {field: 'status', value: 'active'}, filter ) }}
             </div>
           </div>
           <div class="text-center">
             <div>Inactive</div>
             <div>
-              {{ sortGeneralField(users, 'status', 'inactive') }} | {{ sortToday(users, {field: 'createdAt', value: ''}, {field: 'status', value: 'inactive'}).length }}
+              {{ leftFunc(users, {field: 'status', value: 'inactive'}) }} | {{ rightFunc(users, {field: 'createdAt', value: ''}, {field: 'status', value: 'inactive'}, filter) }}
             </div>
           </div>
         </q-card-section>
@@ -68,11 +72,11 @@ export default {
 
   methods: {
     leftFunc(data, query1, query2, filter){
-      if(filter.value == 'day' && filter.type == 'base'){
+      if(filter && filter.value == 'day' && filter.type == 'base'){
         return this.sortToday(data, query1, query2, 'previous').length
-      }else if(filter.value == 'week' && filter.type == 'base'){
+      }else if(filter && filter.value == 'week' && filter.type == 'base'){
         return this.sortWeek(data, query1, query2, 'previous').length
-      }else if(filter.value == 'month' && filter.type == 'base'){
+      }else if(filter && filter.value == 'month' && filter.type == 'base'){
         return this.sortMonth(data, query1, query2, 'previous').length
       }else{
         return this.sortGeneralField(data, {}).length
@@ -80,11 +84,11 @@ export default {
     },
 
     rightFunc(data, query1, query2, filter){
-      if(filter.value == 'day' && filter.type == 'base'){
+      if(filter && filter.value == 'day' && filter.type == 'base'){
         return this.sortToday(data, query1, query2, 'current').length
-      }else if(filter.value == 'week' && filter.type == 'base'){
+      }else if(filter && filter.value == 'week' && filter.type == 'base'){
         return this.sortWeek(data, query1, query2, 'current').length
-      }else if(filter.value == 'month' && filter.type == 'base'){
+      }else if(filter && filter.value == 'month' && filter.type == 'base'){
         return this.sortMonth(data, query1, query2, 'current').length
       }else{
         return this.sortToday(data, query1, query2, 'current').length
--- src/components/users/index.vue
+++ src/components/users/index.vue
@@ -1,12 +1,64 @@
 <template>
   <div>
 
-    <q-table square
-      title="Users"
-      :data="users"
-      :columns="columns"
-      row-key="name"
-    />
+    <q-table square title="Users" :data="users" :columns="columns" row-key="name"  :filter="filter">
+
+      <template v-slot:top-right>
+        <q-input outlined dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+        <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="name" :props="props">
+            {{ props.row.name }}
+          </q-td>
+          <q-td key="phone" :props="props">
+              {{ props.row.phone }}
+          </q-td>
+          <q-td key="email" :props="props">
+              {{ props.row.email }}
+          </q-td>
+          <q-td key="location" :props="props">
+              {{ props.row.location }}
+          </q-td>
+          <q-td key="gender" :props="props">
+              {{ props.row.gender }}
+          </q-td>
+          <q-td key="verified" :props="props">
+            <q-badge :color="props.row.verified ? 'primary' : 'negative'">
+              {{ props.row.verified }}
+            </q-badge>
+          </q-td>
+           <q-td key="status" :props="props">
+            <q-badge :color="props.row.status == 'active' ? 'primary' : 'negative'">
+              {{ props.row.status }}
+            </q-badge>
+          </q-td>
+
+          <q-td key="agent" :props="props">
+            <q-badge>
+              {{ props.row.agent }}
+            </q-badge>
+          </q-td>
+          <q-td key="createdAt" :props="props">
+              {{ formatDate(props.row.createdAt) }}
+          </q-td>
+          <q-td key="date_of_birth" :props="props">
+              {{ formatDate(props.row.date_of_birth) }}
+          </q-td>
+          <q-td key="action" :props="props" class="q-gutter-xs">
+              <q-btn size="sm" no-caps color="negative"  label="Deactivate" />
+              <q-btn size="sm" no-caps color="secondary" label="Make Agent" />
+          </q-td>
+
+        </q-tr>
+      </template>
+
+    </q-table>
 
   </div>
 </template>
@@ -17,6 +69,7 @@ export default {
   // name: 'ComponentName',
   data () {
     return {
+      filter: '',
       columns: [
         {
           name: 'name',
@@ -36,7 +89,8 @@ export default {
         { name: 'agent', label: 'Agent', field: 'agent', sortable: true },
         // { name: 'subscribed', label: 'Subscribed', field: 'subscribed', sortable: true },
         { name: 'createdAt', label: 'Join Date', field: 'createdAt', sortable: true, format: val => `${date.formatDate(val, 'YYYY-MM-DD')}` },
-        { name: 'date_of_birth', label: 'Birth Date', field: 'date_of_birth', format: val => `${date.formatDate(val, 'YYYY-MM-DD')}`, }
+        { name: 'date_of_birth', label: 'Birth Date', field: 'date_of_birth', format: val => `${date.formatDate(val, 'YYYY-MM-DD')}`, },
+        { name: 'action', label: 'Action', field: '' },
       ]
     }
   },
@@ -45,6 +99,12 @@ export default {
     users() {
       return this.$store.getters['DataAuth/users'];
     },
+  },
+
+  methods: {
+    formatDate(data){
+      return date.formatDate(data, 'YYYY-MM-DD')
+    }
   },
 }
 </script>
