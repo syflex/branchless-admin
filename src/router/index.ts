b2652eaae3d52d72f21786e4808629bd965ac0d3
Simon Moses Onazi <syflex360@gmail.com>
2020-07-27 14:48:13 +0100

    turn strict to false

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ import routes from './routes';
  * directly export the Router instantiation
  */
 
-export default route<StoreInterface>(function ({ Vue } : {Vue:any}) {
+export default route<StoreInterface>(function ({ Vue }) {
   Vue.use(VueRouter);
 
   const Router = new VueRouter({
