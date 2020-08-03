f784f78bb183a3c83f6404710b76ae5366557854
Simon Moses Onazi <syflex360@gmail.com>
2020-08-03 12:17:56 +0100

    updated admin

--- src/components/users/details.vue
+++ src/components/users/details.vue
@@ -2,6 +2,9 @@
   <div>
     {{user}}
     {{userWallet}}
+    {{userSavings}}
+    {{userWalletTrans}}
+    {{userBankTrans}}
   </div>
 </template>
 
@@ -12,13 +15,19 @@ export default {
   data () {
     return {
       user: '',
-      userWallet: ''
+      userWallet: '',
+      userSavings: [],
+      userWalletTrans: [],
+      userBankTrans: []
     }
   },
 
   mounted() {
     this.getUser(this.$route.params.phone);
     this.getUserWallet(this.$route.params.phone);
+    this.getUserSavings(this.$route.params.phone);
+    this.getUserWalletTrans(this.$route.params.phone);
+    this.getUserBankTrans(this.$route.params.phone);
   },
 
   methods: {
@@ -36,9 +45,42 @@ export default {
     async getUserWallet(phone){
       this.loading = phone;
       try {
-        const response = await this.$axios.post(process.env.Api + '/admin/walletBalance', { phone: phone });
-        const data = await response.data;
+        const response = await this.$axios.get(process.env.Api + '/admin/walletBalance?phone='+ phone);
+        const data = response.data;
         this.userWallet = data
+      } catch (error) {
+
+      }
+    },
+
+    async getUserSavings(phone){
+      this.loading = phone;
+      try {
+        const response = await this.$axios.get(process.env.Api + '/admin/userSavings?phone='+ phone);
+        const data = await response.data;
+        this.userSavings = data
+      } catch (error) {
+
+      }
+    },
+
+    async getUserWalletTrans(phone){
+      this.loading = phone;
+      try {
+        const response = await this.$axios.get(process.env.Api + '/admin/userWalletTrans?phone='+ phone);
+        const data = response.data;
+        this.userWalletTrans = data
+      } catch (error) {
+
+      }
+    },
+
+    async getUserBankTrans(phone){
+      this.loading = phone;
+      try {
+        const response = await this.$axios.get(process.env.Api + '/admin/userBankTrans?phone='+ phone);
+        const data = response.data;
+        this.userBankTrans = data
       } catch (error) {
 
       }
