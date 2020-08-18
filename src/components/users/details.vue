b41ee2c1d847dabdcc1f5da80f2e36ccb9e5e80c
Simon Moses Onazi <syflex360@gmail.com>
2020-08-18 14:03:31 +0100

    updated user details

--- src/components/users/details.vue
+++ src/components/users/details.vue
@@ -1,10 +1,84 @@
 <template>
   <div>
-    {{user}}
-    {{userWallet}}
+
+    <div class="row q-gutter-xs">
+      <q-field dense color="black" bg-color="white" outlined label="Full Name" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].name }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Phone Number" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].phone }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="UUID" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].uid }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Location" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].location }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Date Of Birth" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].date_of_birth }}</div>
+        </template>
+      </q-field>
+
+      <q-field dense color="black" bg-color="white" outlined label="Gender" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].gender }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Verified" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].verified }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Status" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].status }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Agent" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].agent }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Subscribed" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].subscribed }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Due Date" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].due_date }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Email" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].email }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Date Created" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].createdAt }}</div>
+        </template>
+      </q-field>
+      <q-field dense color="black" bg-color="white" outlined label="Date Updated" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{ user[0].updatedAt }}</div>
+        </template>
+      </q-field>
+
+    </div>
+    <!-- {{userWallet}}
     {{userSavings}}
     {{userWalletTrans}}
-    {{userBankTrans}}
+    {{userBankTrans}} -->
   </div>
 </template>
 
@@ -36,7 +110,7 @@ export default {
       try {
         const response = await this.$axios.post(process.env.Api + '/admin/findUser', { phone: phone });
         const data = await response.data;
-        this.user = data
+        this.user = data.data
       } catch (error) {
 
       }
