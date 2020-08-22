0dc556ef9ed5a8032578df54f721b83fe1187bb0
Simon Moses Onazi <syflex360@gmail.com>
2020-08-23 00:19:36 +0100

    direct login

--- src/components/auth/login.vue
+++ src/components/auth/login.vue
@@ -6,7 +6,7 @@
       </div>
 
       <q-card-section class="q-gutter-sm">
-        <q-input dark outlined dense rounded v-model="form.email" label="Enter username" >
+        <q-input dark outlined dense rounded v-model="form.phone" label="Enter Phone" >
           <template v-slot:prepend>
             <div class="row flex-center">
               <q-icon name="ion-ios-person" />
@@ -14,7 +14,7 @@
             </div>
           </template>
         </q-input>
-        <q-input dark type="password" outlined dense rounded v-model="form.password" label="Enter Your Password" >
+        <q-input dark type="password" outlined dense rounded v-model="form.pin" label="Enter Pin" >
           <template v-slot:prepend>
             <q-icon name="ion-unlock" />
             <div class="q-px-sm">|</div>
@@ -45,27 +45,27 @@ export default {
       isPwd: true,
       loading: false,
       form:{
-        email: '',
-        password: ''
+        phone: '',
+        pin: ''
       }
     }
   },
 
   computed: {
     formIsValid () {
-      return this.form.email !== '' &&
-            this.form.password !== ''
+      return this.form.phone !== '' &&
+            this.form.pin !== ''
     },
   },
 
   methods: {
     async login(){
-        const response = this.$axios.post(process.env.Api+'/admin/login', { phone: this.form.email, pin: this.form.email})
+        const response = await this.$axios.post(process.env.Api+'/admin/login', this.form )
         const data = response.data
         this.$axios.defaults.headers.common['Authorization'] = data.token;
         commit('login', { data: data });
         // await this.$store.dispatch('DataAuth/login');
-        // this.$router.push({name: 'users'});
+        this.$router.push({name: 'users'});
     }
   },
 }
